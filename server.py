2cf99328ef0f0da984be6b2171e35f47e71b50ca
MirindraRandrianarijaona <randrianarijaonamirindra@gmail.com>
2023-03-01 15:43:31 +0300

    Création du tableau contenant les colonnes: album, artist, et genre.

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ urls = (
 
 class index:
     def GET(self):
+<<<<<<< feat/server
         db = web.database(
             dbn='mysql',
             host='tmp-insi.rktmb.org',
@@ -18,8 +19,40 @@ class index:
         a2=db.select('Album', limit=2)
         result = '<html><head><title>test</title></head>'
         result += '<body>'
+=======
+        d = Db()
+        db = d.getDb()
+        a2=db.select('Album', limit=10)
+        albumids=db.select('Album', limit=10)
+        artists=db.select('Artist', limit=10)
+        genres=db.select('Genre', limit=10)
+        result = '<html><head><title>Gestionnaire de musiques</title></head>'
+        result = '<html><head><title>Server.py G03</title>'
+        result += '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">'
+        result += '</head>'
+        result += '<body>'
+        result += nav.nav()
+        result += '<div class="container">'
+        result += '<h2 style="text-align: center;">Liste principale</h2>'
+        result += '<table class="table">'
+        result += '<thead class="table-dark">'
+        result += '<tr><th>Genre</th><th>Artists</th><th>Album</th></tr>'
+        result += '</thead>'
+        result += '<tbody class="table-primary">'
+>>>>>>> local
         for a in a2:
-            result += a.Title + ',(' + str(a.ArtistId) + ') <br/>'
+            result +='<tr>'
+            for genre in genres:
+                result +='<td>'+genre.Name+'</td>'
+                break
+            for artist in artists:
+                result +='<td>'+artist.Name+'</td>'
+                break
+            result +='<td>'+a.Title+'</td>'
+            result +='</tr>'
+        result += '</tbody>'
+        result +='</table>'
+        result +='</div>'
         result += '</body></html>'
         return result
 
