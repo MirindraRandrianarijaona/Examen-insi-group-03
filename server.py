70740efef6cf1091c9911d0360e3c1a2bdd550e8
Andriantsoa RATSIMANDRESY <andriantsoa024@gmail.com>
2023-03-02 13:13:42 +0200

    Ajout de la colonne track au tableau de server.py avec modificatioin de la place des colonnes.

--- server.py
+++ server.py
@@ -17,6 +17,7 @@ class index:
         genres=db.select('Genre', limit=10)
         media_types=db.select('MediaType', limit=10)
         playlists=db.select('Playlist', limit=10)
+        tracks=db.select('Track', limit=10)
         result = '<html><head><title>Gestionnaire de musiques</title></head>'
         result = '<html><head><title>Server.py G03</title>'
         result += '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">'
@@ -27,10 +28,10 @@ class index:
         result += '<h2 style="text-align: center;">Liste principale</h2>'
         result += '<table class="table">'
         result += '<thead class="table-dark">'
-        result += '<tr><th>Id</th><th>Genre</th><th>Artists</th><th>Album</th><th>Media type</th><th>Playlist</th></tr>'
+        result += '<tr><th>Id</th><th>Genre</th><th>Artists</th><th>Album</th><th>Track</th><th>Media type</th><th>Playlist</th></tr>'
         result += '</thead>'
         result += '<tbody class="table-primary">'
-        for a in a2:
+        for playlist in playlists:
             result +='<tr>'
             for albumid in albumids:
                 result +='<td>'+str(albumid.AlbumId)+'</td>'
@@ -41,13 +42,16 @@ class index:
             for artist in artists:
                 result +='<td>'+artist.Name+'</td>'
                 break
+            for a in a2:
+                result +='<td>'+a.Title+'</td>'
+                break
+            for track in tracks:
+                result +='<td>'+track.Name+'</td>'
+                break
             for media_type in media_types:
                 result +='<td>'+media_type.Name+'</td>'
                 break
-            for playlist in playlists:
-                result +='<td>'+playlist.Name+'</td>'
-                break
-            result +='<td>'+a.Title+'</td>'
+            result +='<td>'+playlist.Name+'</td>'
             result +='</tr>'
         result += '</tbody>'
         result +='</table>'
