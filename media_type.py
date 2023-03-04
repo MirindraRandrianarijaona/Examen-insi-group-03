ffcd0796c97f11a2bff0f6a505940ddf65a0cca6
Andriantsoa RATSIMANDRESY <andriantsoa024@gmail.com>
2023-03-04 22:35:13 +0200

    Ajout de la colonne Id dans le tableau media type.

--- media_type.py
+++ media_type.py
@@ -13,6 +13,7 @@ class media:
     def GET(self):
         d = Db()
         db = d.getDb()
+        albumids=db.select('Album', limit=10)
         media_types=db.select('MediaType', limit=10)
         result = '<html><head><title>Media_type.py G03</title>'
         result += '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">'
@@ -23,11 +24,14 @@ class media:
         result += '<h2 style="text-align: center;">Liste des media type</h2>'
         result += '<table class="table">'
         result += '<thead class="table table-dark">'
-        result += '<tr><th>Media type</th></tr>'
+        result += '<tr><th>Id</th><th>Media type</th></tr>'
         result += '</thead>'
         result += '<tbody class="table-primary">'
         for media_type in media_types:
             result +='<tr>'
+            for albumid in albumids:
+                result +='<td>'+str(albumid.AlbumId)+'</td>'
+                break
             result +='<td>'+media_type.Name+'</td>'
             result +='</tr>'
         result += '</tbody>'
